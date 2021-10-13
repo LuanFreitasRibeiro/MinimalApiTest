cf92f8886fcb3a6d839d3694a842930998c07533
Luan Freitas <luannr16@gmail.com>
2021-10-12 21:36:00 -0300

    testing MinimalApi

---
+++ ViewModels/CreateTodoViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+
+public class CreateTodoViewModel : Notifiable<Notification>
+{
+    public string Title { get; set; }
+
+    public Todo MapTo()
+    {
+        //AddNotifications(new Contract<Notification>()
+        //    .Requires()
+        //    .IsNullOrEmpty(Title, "Title is required")
+        //    .IsGreaterOrEqualsThan(Title, 5, "Title must be greater than 5"));
+
+        return new Todo(Guid.NewGuid(), Title, false);
+    }
+}
+
+
